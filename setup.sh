203ff2dfc77a0299878479ad74faf1553bb51c3a
jaytea33 <yerffej@gmail.com>
2023-11-24 23:13:43 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 docker build -t hfs .
 docker run -d --name hfsserver --network bridge -p 8445:80 hfs
+echo "Waiting 20 seconds to allow Docker container to exist before copying config"
+sleep 20
 docker cp config.yaml hfsserver:/root/.hfs/
